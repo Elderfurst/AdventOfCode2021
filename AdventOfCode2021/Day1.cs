99c61c05aebad39db528866376cf05708c0b3f3d
Nick Anderson <njcanderson@gmail.com>
2021-12-02 09:38:05 -0500

    Day 1 input and solutions

---
+++ AdventOfCode2021/Day1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021
+{
+    public class Day1
+    {
+        private static readonly int[] Inputs = File.ReadAllLines(@"Inputs/Day1.txt").Select(int.Parse).ToArray();
+        public static void Run()
+        {
+			PartOne();
+            PartTwo();
+        }
+
+        private static void PartOne()
+		{
+            var totalIncreases = 0;
+
+            for (var i = 1; i < Inputs.Length; i++)
+			{
+                if (Inputs[i - 1] < Inputs[i])
+				{
+                    totalIncreases++;
+				}
+			}
+
+            Console.WriteLine(totalIncreases);
+		}
+
+        private static void PartTwo()
+		{
+            var totalIncreases = 0;
+
+            var index = 0;
+
+            while (true)
+			{
+                if (index + 3 >= Inputs.Length)
+				{
+                    break;
+				}
+
+                var oneOne = Inputs[index];
+                var oneTwo = Inputs[index + 1];
+                var oneThree = Inputs[index + 2];
+
+                var twoOne = Inputs[index + 1];
+                var twoTwo = Inputs[index + 2];
+                var twoThree = Inputs[index + 3];
+
+                var oneSum = oneOne + oneTwo + oneThree;
+                var twoSum = twoOne + twoTwo + twoThree;
+
+                if (oneSum < twoSum)
+				{
+                    totalIncreases++;
+				}
+
+                index++;
+			}
+
+            Console.WriteLine(totalIncreases);
+		}
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2021
+{
+	public class Program
+	{
+        public static void Main()
+        {
+            var run = new Day2();
+			Day2.Run();
+        }
+    }
+}
